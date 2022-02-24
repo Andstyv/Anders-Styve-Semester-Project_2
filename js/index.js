4ca3652b5e46f28ff4ba06fabdf79981197c63e1
Andstyv <74601625+Andstyv@users.noreply.github.com>
2022-02-24 16:34:32 +0100

    Nav, main and footer styling

--- js/index.js
+++ js/index.js
@@ -36,14 +36,14 @@ fetchFeaturedProducts();
 async function fetchHeaderImg() {
   try {
     const headerImgUrl = url + "/home/";
-    const img = document.querySelector(".bg-img");
+    const img = document.querySelector(".bg-grid__headerimg");
 
     const response = await fetch(headerImgUrl);
     const json = await response.json();
 
     let headerImg = json;
 
-    img.innerHTML = `<img class="img1" src="${url}${headerImg.hero_banner.url}" alt="${headerImg.hero_banner_alt_text}" />`;
+    img.innerHTML = `<img class="bg-grid__headerimg--img" src="${url}${headerImg.hero_banner.url}" alt="${headerImg.hero_banner_alt_text}" />`;
     console.log(headerImg);
   } catch (error) {
     console.log(error);
--- js/modules/createNavMenu.js
+++ js/modules/createNavMenu.js
@@ -17,16 +17,16 @@ export function createNavMenu() {
 
   navContainer.innerHTML = `
   <div class="nav-content">
-  <div class="nav__logo"><i class="fab fa-pushed"></i><p class="nav__logo--txt">shoos</p></div>
-  <div class="nav__left">
-    <ul class="nav__list">
-      <li class="nav__item"><a href="/" class="${pathname === "/" ? "nav__link--active" : "nav__link"}">Home</a></li>
-      <li class="nav__item"><a href="products.html"class="${pathname === "/products.html" ? "nav__link--active" : "nav__link"}">Products</a></li>
-      <li class="nav__item"><a href="cart.html"class="${pathname === "/cart.html" ? "nav__link--active" : "nav__link"}">Cart</a></li>
+  <div class="nav-content__logo"><i class="fab fa-pushed"></i><p class="nav-content__logo--txt">shoos</p></div>
+  <div class="nav-content__main">
+    <ul class="nav-content__list">
+      <li class="nav-content__item"><a href="/" class="${pathname === "/" ? "nav-content__link--active" : "nav-content__link"}">Home</a></li>
+      <li class="nav-content__item"><a href="products.html"class="${pathname === "/products.html" ? "nav-content__link--active" : "nav-content__link"}">Products</a></li>
+      <li class="nav-content__item"><a href="cart.html"class="${pathname === "/cart.html" ? "nav-content__link--active" : "nav-content__link"}">Cart</a></li>
     </ul>
   </div>
-<div class="nav__right">
-<ul class="nav__list--admin">
+<div class="nav-content__admin">
+<ul class="nav-content__adminlist">
 ${authLink}
 ${logoutLink}
 </ul>
