27f8fed6f2872a0412359538e1a4a02fb931b930
Andstyv <74601625+Andstyv@users.noreply.github.com>
2022-02-25 15:49:36 +0100

    Add new product styling

--- js/modules/addNewProduct.js
+++ js/modules/addNewProduct.js
@@ -14,13 +14,15 @@ export function addNewProduct() {
     const addBtn = document.getElementById("add-new");
 
     addBtn.onclick = function () {
+      addBtn.style.display = "none";
       addContainer.innerHTML += `
           <div id="add-error"></div>
           <form class="add-form">
           <button class="btn-main" id="add-close-btn">Close</button>
+          <h3 class="add-form__title">Add a new product below</h3>
           <div>
           <label for="productNameInput">Product Name</label>
-          <input type="text" id="productNameInput" placeholder="Product Name" />
+          <input type="text" minlength="4" required id="productNameInput" placeholder="Product Name" />
         </div>
         <div>
           <label for="toggleFeatured">Featured</label>
@@ -28,11 +30,11 @@ export function addNewProduct() {
         </div>
         <div>
           <label for="priceInput">Product Price</label>
-          <input type="number" step="0.01" id="priceInput" placeholder="Product Price" />
+          <input type="number" step="0.01" required id="priceInput" placeholder="Product Price" />
         </div>
         <div>
           <label for="descInput">Description</label>
-          <textarea type="text" id="descInput" placeholder="Description"></textarea>
+          <textarea type="text" minlength="10" required id="descInput" placeholder="Description"></textarea>
         </div>
         <div class="edit-conf-msg"></div>
 <button type="submit" class="btn-secondary" id="add-btn">Add</button>
@@ -40,6 +42,7 @@ export function addNewProduct() {
       const form = document.querySelector("form");
       const closeBtn = document.getElementById("add-close-btn");
       closeBtn.onclick = function () {
+        addBtn.style.display = "flex";
         addContainer.innerHTML = `<button class="btn-main" id="add-new">+ Add new product</button>`;
         addNewProduct();
       };
